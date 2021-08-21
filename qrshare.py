f49e7b7e20449767798d7c844a73b7a3fc4f084b
Kesavan M <code@k7esavan.com>
2021-08-21 22:33:56 +0530

    maded some designs

--- qrshare.py
+++ qrshare.py
@@ -1,11 +1,32 @@
 
 import tkinter as tk
+from tkinter import filedialog
+import PIL
+from PIL import Image, ImageTk
+import qrcode
+import cv2
+
+def getQRCodeFileName():
+    return filedialog.askopenfilename(
+        initialdir='/',
+        title='Select the file',
+        filetypes=(('PNG files', '*.png'),
+                   ('JPG files', '*.jpg'),
+                   ('JPEG files','*.jpeg'))
+    )
+
+def getFileName():
+    return filedialog.askopenfilename(initialdir='/')
+
+def writeDataIntoFile(outputfile):
+    pass
 
 def main():
     window = tk.Tk()
     window.title('QRShare')
     window.geometry('1395x933')
     window.configure(bg='#FFFFFF')
+    window.resizable(False, False)
 
     canvas = tk.Canvas(
             master=window,
@@ -22,6 +43,46 @@ def main():
 
     # For getting input
     canvas.create_rectangle(707.0, 0.0, 1395.0, 933.0, fill='#FFFFFF', outline='')
+
+    select_button_image = tk.PhotoImage(file='./resources/buttons/select_btn.png')
+    exit_button_image = tk.PhotoImage(file='./resources/buttons/exit_btn.png')
+
+    select_button = tk.Button(
+        master=window,
+        image=select_button_image,
+        borderwidth=0,
+        highlightthickness=0,
+        command=getQRCodeFileName,
+        relief='flat'
+    )
+
+    exit_button = tk.Button(
+        master=window,
+        image=exit_button_image,
+        borderwidth=0,
+        highlightthickness=0,
+        command=window.destroy,
+        relief='flat'
+    )
+
+    select_button.place(
+        x=753.0,
+        y=544.0
+    )
+
+    exit_button.place(
+        x=753.0,
+        y=686.0
+    )
+
+    # Welcome message
+    welcomeMessage = ImageTk.PhotoImage(Image.open('./resources/welcomeMessage.png'))
+    welcome_label = tk.Label(
+            master=window,
+            height=500,
+            width=500,
+            image=welcomeMessage)
+    welcome_label.place(x = 50, y=10)
 
     window.mainloop()
 if __name__ == '__main__':
